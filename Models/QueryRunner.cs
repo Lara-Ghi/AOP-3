9b9d3760c89a82d0006f8407b9470c26b9dcfdc0
chaeyrie <carolinafnunesamf@gmail.com>
2025-03-28 14:09:59 +0100

    bar chart top genres

--- Models/QueryRunner.cs
+++ Models/QueryRunner.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 public class QueryRunner 
 {
-    public (List<double> Counts, List<string> Names)   Run_Generic_Bar_Chart() 
+    public (List<double> Counts, List<string> Names) Run_Platform_Bar_Chart() 
     {
         DataLoader<GlobalMusicStreamingModel> musicLoader = new DataLoader<GlobalMusicStreamingModel>();
         musicLoader.LoadData("CSV-Files/globalMusicStreaming.csv");
@@ -22,5 +22,25 @@ public class QueryRunner
         List<double> counts = platform_counts.Select(p => (double)p.Count).ToList();
         List<string> names = platform_counts.Select(p => p.Platform).ToList()!;
         return (counts, names);
+    }
+
+    public (List<double> GenreCounts, List<string> GenreNames) Run_Genre_Bar_Chart()
+    {
+        DataLoader<GlobalMusicStreamingModel> musicLoader = new DataLoader<GlobalMusicStreamingModel>();
+        musicLoader.LoadData("CSV-Files/GlobalMusicStreaming.csv");
+
+        var musicData = musicLoader.data;
+
+        var genre_counts = musicData.GroupBy(p => p.TopGenre)
+                                                .Select(p => new
+                                                {
+                                                    Genre = p.Key,
+                                                    Count = p.Count()
+                                                })
+                                                .OrderByDescending(p => p.Count);
+        
+        List<double> genrecounts = genre_counts.Select(p => (double)p.Count).ToList();
+        List<string> genrenames = genre_counts.Select(p => p.Genre).ToList()!;
+        return (genrecounts, genrenames);
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -8,6 +8,7 @@ using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
 using LiveChartsCore.Themes;
 using System;
+using SkiaSharp;
 
 namespace AOP_3.ViewModels;
 
@@ -16,11 +17,13 @@ public partial class MainWindowViewModel : ViewModelBase
     [ObservableProperty]
     private string output = String.Empty;
     public ISeries[] Series { get; set; } = [];
+    public ISeries[] GenreSeries { get; set; } = [];
     public Axis[] YAxes { get; set; } = [];
+    public Axis[] GenreYAxes { get; set; } = [];
     public MainWindowViewModel()
     {
         QueryRunner queryRunner = new QueryRunner();
-        (List<double> counts, List<string> names) = queryRunner.Run_Generic_Bar_Chart();
+        (List<double> counts, List<string> names) = queryRunner.Run_Platform_Bar_Chart();
         Series = [
             new RowSeries<double>
             {
@@ -35,5 +38,19 @@ public partial class MainWindowViewModel : ViewModelBase
             }
         ];
 
+        (List<double> genrecounts, List<string> genrenames) = queryRunner.Run_Genre_Bar_Chart();
+        GenreSeries = [
+            new RowSeries<double>
+            {
+                Values = genrecounts,
+            }
+        ];
+
+        GenreYAxes = [
+            new Axis
+            {
+                Labels = genrenames,
+            }
+        ];
     }
 }
